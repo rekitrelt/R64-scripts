4c5e4986cae2312d5ab78f3a40f574bdfcc402be
rekitrelt <67936854+rekitrelt@users.noreply.github.com>
2021-12-15 22:28:33 -0800

    Update Level Template.lua

--- Custom Levels/Level Template.lua
+++ Custom Levels/Level Template.lua
@@ -1,10 +1,10 @@
-game:GetObjects("rbxassetid://8255433130")[1].Parent = game.ReplicatedFirst.maps
+game:GetObjects("rbxassetid://ID")[1].Parent = game.ReplicatedFirst.maps
 SpawnPos = Vector3.new(0, 900, 0)
 
 fold = workspace.plam:WaitForChild(game.Players.LocalPlayer.Name)
 prtl = game.ReplicatedFirst.TeleMap:Clone()
 text = fold.map.Value
-prtl.Parent = workspace
+prtl.Parent = workspace[text]
 prtl.Transparency = 0
 prtl.Position = workspace.char.Position+Vector3.new(0,6.5,0)
 prtl.to.Value = "custom"
