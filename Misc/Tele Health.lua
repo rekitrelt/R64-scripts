d4b3f9c82d7b44ee6996fc4a3d0e5bf2b60cb091
rekitrelt <67936854+rekitrelt@users.noreply.github.com>
2021-12-16 20:46:35 -0800

    updated to be draggable

--- Misc/Tele Health.lua
+++ Misc/Tele Health.lua
@@ -2,6 +2,12 @@ local healthgui = Instance.new("ScreenGui")
 healthgui.Name = "healthgui"
 healthgui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
+local dragframe = Instance.new("Frame")
+dragframe.Name = "dragframe"
+dragframe.Parent = healthgui
+dragframe.BackgroundTransparency = 1
+dragframe.Size = UDim2.new(1, 0, 1, 0)
+
 local number = Instance.new("ImageLabel")
 number.Name = "number"
 number.Size = UDim2.new(0.0732064, 0, 0.1303781, 0)
@@ -11,12 +17,17 @@ number.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 number.ResampleMode = Enum.ResamplerMode.Pixelated
 number.ImageRectSize = Vector2.new(21, 20)
 number.Image = "rbxassetid://8262464941"
-number.Parent = healthgui
+dragframe.Parent = healthgui
+number.Parent = dragframe
 
 local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
 UIAspectRatioConstraint.Parent = number
 
-healthgui.Parent = game:GetService('Players').LocalPlayer:WaitForChild('PlayerGui').UI
+healthgui.Parent = game.CoreGui
+
+dragframe.Selectable = true
+dragframe.Active = true
+dragframe.Draggable = true
 
 local img = number
 
